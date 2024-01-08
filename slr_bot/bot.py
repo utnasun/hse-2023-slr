21556ab52561df90f2e00994435f7b32602110e8
asvistunov <aleksandrsvistunov4@gmail.com>
2024-01-08 17:42:47 +0300

    Add demo bot

---
+++ slr_bot/bot.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from dotenv import load_dotenv
+
+load_dotenv('../.env')
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=os.environ['BOT_TOKEN'])
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
